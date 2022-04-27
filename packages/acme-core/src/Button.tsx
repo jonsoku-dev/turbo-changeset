9a5c976155d144e366327dddd0a11bf28f065f30
jongseok-lee <jongseok.lee@linecorp.com>
2022-04-27 18:52:57 +0900

    chore(core): v0.0.3 test

--- packages/acme-core/src/Button.tsx
+++ packages/acme-core/src/Button.tsx
@@ -4,7 +4,7 @@ export interface ButtonProps {
   children: React.ReactNode;
 }
 
-console.log('core test')
+console.log('core test v3')
 
 export function Button(props: ButtonProps) {
   return <button>{props.children}</button>;
