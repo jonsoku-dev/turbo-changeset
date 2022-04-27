dc860b3d29348f9e120a53391848247e77b0e6fb
jongseok-lee <jongseok.lee@linecorp.com>
2022-04-27 18:10:44 +0900

    feat: any

---
+++ apps/docs/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@acme/core";
+import { useIsomorphicLayoutEffect } from "@acme/utils";
+
+export default function Docs() {
+  console.log('test')
+  useIsomorphicLayoutEffect(() => {
+    console.log("Acme docs page");
+  }, []);
+  return (
+    <div>
+      <h1>Acme Documentation</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
